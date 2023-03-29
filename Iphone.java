a15785d258fcfaf0a3a52cc03044d9112bb23516
Sridurga Srinivasan <129294106+SridurgaSrinivasan@users.noreply.github.com>
2023-03-29 19:07:52 +0530

    Add files via upload
    Assignment1

---
+++ Iphone.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Iphone {
+	public void makeCall() {
+		// m1
+		System.out.println("Function to makecall");
+
+	}
+	
+	public void model() {
+		// m2
+		System.out.println("Function to find model");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
