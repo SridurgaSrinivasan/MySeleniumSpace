e00c46ccc5851181bc1251cee7b20d49c0dfe0c3
Sridurga Srinivasan <129294106+SridurgaSrinivasan@users.noreply.github.com>
2023-03-29 19:08:10 +0530

    Add files via upload

---
+++ Smartphone.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// calling Mobile class here 
+		Iphone device= new Iphone();
+		device.makeCall();
+		device.model();;
+
+	}
+
+}
